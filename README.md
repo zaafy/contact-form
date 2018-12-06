45920628176c2846778806ab351929b201bcc628
Daniel Okoń <wizualizacje.zaaf@gmail.com>
2018-12-06 23:57:19 +0100

    Add form, add readme, test if gulp works correctly

---
+++ README.md
@@ -0,0 +1,8 @@
+# contact-form
+Contact form with SASS and AJAX
+To run please follow these steps:
+1. Download or clone
+2. Put it on localhost or some hosting
+3. Run npm install
+4. Start compilation with gulp
+5. Ready
--- index.php
+++ index.php
@@ -7,5 +7,17 @@
 </head>
 <body>
 
+	<form class="contact-form">
+	  <input type="text" name="name" placeholder="Name and Surname" class="contact-form__text-input">
+	  <input type="email" name="email" placeholder="email@address.com" class="contact-form__text-input">
+	  <select name="subject" class="contact-form__select">
+	    <option value="1">First option</option>
+	    <option value="2">Second option</option>
+	    <option value="3">Third option</option>
+	  </select>
+	  <textarea rows="4" name="message" placeholder="Please write Your request here."></textarea>
+	  <input type="submit" value="Send Request" class="contact-form__submit">
+	</form>
+	
 </body>
 </html>
