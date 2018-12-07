f8fba25244178181f7d26905a7f774d828ded824
Daniel Okoń <dan.okon77@gmail.com>
2018-12-08 00:29:47 +0100

    add further styles to form

--- index.php
+++ index.php
@@ -1,23 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Contact Form</title>
-	<link rel="stylesheet" href="css/style-global.css">
-</head>
-<body>
-	<div class="container">
-		<form class="contact-form">
-		  <input type="text" name="name" placeholder="Name and Surname" class="contact-form__text-input">
-		  <input type="email" name="email" placeholder="email@address.com" class="contact-form__text-input">
-		  <select name="subject" class="contact-form__select">
-		    <option value="1">First option</option>
-		    <option value="2">Second option</option>
-		    <option value="3">Third option</option>
-		  </select>
-		  <textarea rows="4" name="message" placeholder="Please write Your request here." class="contact-form__textarea"></textarea>
-		  <input type="submit" value="Send Request" class="contact-form__submit">
-		</form>
-	</div>
-</body>
+	<head>
+		<meta charset="UTF-8">
+		<title>Contact Form</title>
+		<link rel="stylesheet" href="css/style-global.css">
+	</head>
+	<body>
+		<div class="container">
+			<form class="contact-form">
+
+				<h1 class="contact-form__title">Contact Form</h1>
+
+				<input type="text" name="name" placeholder="Name and Surname" class="contact-form__text-input">
+				<input type="email" name="email" placeholder="email@address.com" class="contact-form__text-input">
+				<select name="subject" class="contact-form__select">
+					<option value="1">First option</option>
+					<option value="2">Second option</option>
+					<option value="3">Third option</option>
+				</select>
+				<textarea rows="4" name="message" placeholder="Please write Your request here." class="contact-form__textarea"></textarea>
+				<a href="#!" class="contact-form__submit">Send Request</a>
+			</form>
+		</div>
+	</body>
+
+	<link href="https://fonts.googleapis.com/css?family=Merriweather:300,700" rel="stylesheet">
+
 </html>
